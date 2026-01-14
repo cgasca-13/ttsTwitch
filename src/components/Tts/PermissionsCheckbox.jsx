15ad0eb298ec515a0a9e550a49244b8a68dd73c3
C Gasca <carlosgascac@gmail.com>
2026-01-14 13:10:45 -0600

    Add pointer cursor to permissions checkbox
    Updated the checkbox input in PermissionsCheckbox to use a pointer cursor on hover, improving user experience and indicating interactivity.

--- src/components/Tts/PermissionsCheckbox.jsx
+++ src/components/Tts/PermissionsCheckbox.jsx
@@ -23,7 +23,7 @@ export const PermissionsCheckbox = ({ permissions, onPermissionChange }) => {
               id={id}
               checked={permissions[key]}
               onChange={(e) => onPermissionChange(key, e.target.checked)}
-              className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              className="mr-2 w-4 h-4 hover:cursor-pointer text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
             />
             <label htmlFor={id} className="dark:text-white text-gray-800 cursor-pointer">
               {label}
